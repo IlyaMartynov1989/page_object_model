1356f1d800d13d35966aa24b32e70282dcc162f5
Ilya Martynov <ilya.martynov@balance-pl.ru>
2021-09-08 14:46:30 +0300

    add check logan pages

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,13 +8,13 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert BasePage.is_element_present(MainPageLocators.LOGIN_LINK),\
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), \
             "Ссылка регистрации/входа не найдена"
 
     def should_be_login_form(self):
-        assert BasePage.is_element_present(LoginPageLocators.LOGIN_FORM),\
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM),\
             "Форма логин не найдена."
 
     def should_be_register_form(self):
-        assert BasePage.is_element_present(LoginPageLocators.REGISTRATION_FORM),\
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM),\
             "Форма регистрации не найдена."
